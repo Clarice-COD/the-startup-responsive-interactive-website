d9570fee1dfe1a291f6ce26c85908c49c68c2fb3
Clarice-COD <clariceboerlijst@gmail.com>
2025-01-19 20:51:02 +0100

    Micro interaction: clickable slider
    #6

---
+++ index.js
@@ -0,0 +1,18 @@
+// CLICKABLE SLIDER
+
+    // STEP 1 - Select the elements
+    const slider = document.querySelector(".slider");  // Selects the class: slider and names it slider
+    const item = document.querySelector(".slider"); // Selects the class: slider and names it item
+    const itemWidth = item.offsetWidth; // Selects the width of the class item
+
+    // STEP 2 - Add a function
+    function handleClick(direction) {  // handleClick checks which of the 2 button was pressed and bases the next move on that
+            // STEP 3 - Add an action
+            if(direction === "previous") { // Is to compare 2 values of which only one should be used in this case
+                slider.scrollBy({ left: -itemWidth, behavior: "smooth"}); // When the button previous is pressed, the elements will be scrollen to the left
+            }
+
+            else{
+                slider.scrollBy({ left: itemWidth, behavior: "smooth"}); // When the button previous is pressed, the elements will be scrollen to the right
+            }
+    } 
