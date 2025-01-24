261ea78465e0999a16a3b9e4a7782b9bfcc97017
Clarice-COD <clariceboerlijst@gmail.com>
2025-01-24 11:00:13 +0100

    JS slider button click verbeterd

--- route-planner.js
+++ route-planner.js
@@ -1,17 +1,19 @@
 // CLICKABLE SLIDER
     // STEP 1 - Select the elements
     const slider = document.querySelector(".slider");  // Selects the class: slider and names it slider
-    const item = document.querySelector(".slider"); // Selects the class: slider and names it item
-    const itemWidth = item.offsetWidth; // Selects the width of the class item
+    const item = document.querySelector(".slider li"); // Selects the li
+    const btnPrev = document.querySelector(".button--previous") // Selects the class of the button that go's to the previous slide
+    const btnNext = document.querySelector(".button--next") // Selects the class of the button that go's to the next slide
+
+    const itemWidth = item.offsetWidth; // Selects the width of the class item, which is the width of the li
 
     // STEP 2 - Add a function
-    function handleClick(direction) {  // handleClick checks which of the 2 button was pressed and bases the next move on that
-        
-            // STEP 3 - Add an action
-            if(direction === "previous") { // Is to compare 2 values of which only one should be used in this case
-                slider.scrollBy({ left: -itemWidth, behavior: "smooth"}); // When the button previous is pressed, the elements will be scrollen to the left
-            }
-            else{
-                slider.scrollBy({ left: itemWidth, behavior: "smooth"}); // When the button previous is pressed, the elements will be scrollen to the right
-            }
-    } 
+    btnPrev.addEventListener('click', () =>  { // Click event aan de button previous toegevoegd
+        // STEP 3 - Add an action
+        slider.scrollBy({ left: -itemWidth, behavior: "smooth"}); // scr
+    });
+
+    btnNext.addEventListener('click', () =>  { // Click event aan de button next toegevoegd
+        // STEP 3 - Add an action
+        slider.scrollBy({ left: itemWidth, behavior: "smooth"});
+    });
